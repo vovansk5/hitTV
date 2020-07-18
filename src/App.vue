caaa0384a0578113264b7018c9b233d844abfc9f
vovansk5 <vovansk5@gmail.com>
2020-07-18 11:40:35 +0700

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id='App' :style="{background:testColor}" >
+  <div id='App' :class="{fon1:isFon1, fon2:isFon2, fon3:isFon3}" >
      
      <img :src="mainImg" />
      <br>
-     <button @click="textMk">Текст</button>
-     <button @click="textStop">Стоп</button>
-     <button @click="changeBG">Сменить фон</button>
-     <button @click="changeMainImg">Сменить колонку</button>
+     <button @click="textMk">{{btnTextName}}</button>
+     <button id="btnView" @click="textStop" v-show="textShow" >{{btnTextStopName}}</button>
+     <button id="btnChangeBG" @click="changeBG">Next backGR </button>
+     <button id="btnChangeMainImg" @click="changeMainImg">Next set</button>
      <br>
      <input id="inputText" v-model="redTextOriginal" v-show="!textShow">
+
       <marquee id='redTextR' class='redText' direction="right" v-show="runTextFlag" > 
           {{redText}}
       </marquee>
@@ -16,7 +17,9 @@
       <div id='redTextS' class='redText' v-show="textStopFlag" > 
           {{redText}}
       </div>
-      <br>
+
+
+
 
 
   </div>
@@ -41,10 +44,21 @@ export default {
       radiusSetFlag:false,
       circleType:null,
       curBG:0,
-      imgBG: ['url("../public/img/fon1.jpg") no-repeat','../public/img/fon2.png',"../public/img/fon3.png"],
-      testColor:' url("../public/img/fon1.jpg")',
+      imgBG: ['url("./img/fon1.jpg")  no-repeat ','url("./img/fon4.jpg") no-repeat','url("./img/fon3.jpg") no-repeat'],
+      curFon:'',
       testColor2:'3px solid green',
-      mainImg:'./img/hit3.png'
+      mainImg:'./img/hit3.png',
+      BG: {
+        background:'url("./img/fon2.jpg") no-repeat',
+        backgroundSize: '100%'
+      },
+      isFon1:false,
+      isFon2:true,
+      isFon3:false,
+      isFon4:false,
+
+      btnTextName:'New Text',
+      btnTextStopName:'View'
 
     }
   },
@@ -62,19 +76,27 @@ export default {
   methods:{
     textMk() {
       this.textShow= !this.textShow;
+      if (this.textShow)
+        this.btnTextName='New Text'
+      else
+        this.btnTextName='Start';
       this.textStopFlag=false;
       this.circle.destroy.bind(this.circle)
 
     },
     textStop() {
-      this.textStopFlag=true;
+      this.textStopFlag=!this.textStopFlag;
+      if (this.textStopFlag)
+        this.btnTextStopName='Start'
+      else
+        this.btnTextStopName='View'
       //new CircleType(document.getElementById('demo3')).radius(3200);
 
       //this.circleType.destroy();
       //if (!this.radiusSetFlag) {
         document.getElementById('redTextS').innerHTML=this.redText;
         setTimeout(() => {
-           this.circleType = new CircleType(document.getElementById('redTextS')).radius(3200);
+           this.circleType = new CircleType(document.getElementById('redTextS')).radius(3700);
           }, 1
         );
         this.radiusSetFlag=1;
@@ -87,7 +109,23 @@ export default {
       this.curBG++;
       if (this.curBG>2) {
         this.curBG=0;
-      } 
+      }
+      this.curFon=this.imgBG[this.curBG];
+      this.BG.background=this.curFon;
+
+      this.isFon1=false;
+      this.isFon2=false;
+      this.isFon3=false;
+      this.isFon4=false;
+      if (this.curBG==0)
+        this.isFon1=true
+      else if (this.curBG==1)
+        this.isFon2=true
+      else if (this.curBG==2)
+        this.isFon3=true;
+      else if (this.curBG==3)
+        this.isFon4=true;
+
 
     },
     changeMainImg() {
@@ -106,6 +144,23 @@ export default {
 </script>
 
 <style>
+#btnChangeBG {
+  position:absolute;
+  left: 70%;
+  top:20%;
+}
+
+#btnChangeMainImg {
+  position:absolute;
+  left: 70%;
+  top:30%;
+
+}
+
+#btnView {
+  position:relative;
+  top:7px;
+}
 
 #DT {
   background: hotpink;
@@ -165,15 +220,30 @@ body {
   text-align: center;
 }
 
-#App {
+.fon1 {
   background: url("../public/img/fon1.jpg") ; 
   background-size: 100% Auto;
 }
+ 
+.fon2 {
+  background: url("../public/img/fon2.png") ; 
+  background-size: 100% Auto;
+}
 
+.fon3 {
+  background: url("../public/img/fon3.png") ; 
+  background-size: 100% Auto;
+}
+
+.fon4 {
+  background: url("../public/img/fon4.jpg") ; 
+  background-size: 100% Auto;
+}
 
 button {
 display: inline-block;
 font-size: 1.1em;
+height: 50px;
 width:90px;
 text-decoration:none;
 text-align:center;
@@ -200,7 +270,7 @@ transition: ease .15s all;
  -o-animation: none;
   animation: none;
 }
-button:hover, button:focus{
+button:hover{
 -webkit-animation: linear 1.2s light infinite;
 -moz-animation: linear 1.2s light infinite;
 -o-animation: linear 1.2s light infinite;
